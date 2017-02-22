30dad8087628cdebc633cc84bba091d9dcf8ffcd
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-22 18:09:07 -0300

    fix

--- app/services/data_importer.rb
+++ app/services/data_importer.rb
@@ -19,14 +19,14 @@ module Services
       if order.instance_of? Order
         order.update_attributes params
       else
-        Order.create(params)
+        Order.create!(params)
       end
     end
 
-    def update_orders(orders, wordpress)
+    def update_orders(orders, wordpress_id)
       orders.each do |order_json|
         p "--> Atualizando pedido: #{order_json['number']}"
-        save_order(create_order_params(order_json.merge(wordpress: wordpress)))
+        save_order(create_order_params(order_json).merge(wordpress: Wordpress.find(wordpress_id)))
       end
     end
 
