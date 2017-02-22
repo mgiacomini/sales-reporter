a33be5d7f4f4165fefc30d5662ea503d38792a83
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-22 14:50:10 -0300

    fix reports numeric overflow

--- db/migrate/20170222161749_create_reports.rb
+++ db/migrate/20170222161749_create_reports.rb
@@ -1,12 +1,12 @@
 class CreateReports < ActiveRecord::Migration[5.0]
   def change
     create_table :reports do |t|
-      t.decimal :sales, precision: 5, scale: 2
+      t.decimal :sales, precision: 10, scale: 2
       t.integer :orders
       t.integer :items
-      t.decimal :tax, precision: 5, scale: 2
-      t.decimal :shipping, precision: 5, scale: 2
-      t.decimal :discount, precision: 5, scale: 2
+      t.decimal :tax, precision: 10, scale: 2
+      t.decimal :shipping, precision: 10, scale: 2
+      t.decimal :discount, precision: 10, scale: 2
       t.integer :customers
 
       t.timestamps
