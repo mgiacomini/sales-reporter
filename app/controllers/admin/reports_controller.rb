abec3bb59470d1b88e897454265866eb0750ab1b
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-23 12:39:35 -0300

    show only reports of last 30 days

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class ReportsController < ApplicationController
 
     def index
-      @orders = Order.all
+      @orders = Order.last_30_days
       super
     end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :wordpress
+  scope :last_30_days, -> { where("wordpress_created_at < ?", 30.days.ago) }
 end
