3ab29ff3bcf7074b6e7b122b0a0f6617544dbee6
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-23 11:05:53 -0300

    fix date on import

--- app/services/data_importer.rb
+++ app/services/data_importer.rb
@@ -4,10 +4,10 @@ module Services
   class DataImporter
 
     def create_order_params(order_json={})
-      created_at = Date.parse order_json.delete('date_created').to_date
-      updated_at = Date.parse order_json.delete('date_modified').to_date
-      paid_at = Date.parse order_json.delete('date_paid').to_date
-      completed_at = Date.parse order_json.delete('date_completed').to_date
+      created_at = DateTime.parse order_json.delete('date_created').to_date
+      updated_at = DateTime.parse order_json.delete('date_modified').to_date
+      paid_at = DateTime.parse order_json.delete('date_paid').to_date
+      completed_at = DateTime.parse order_json.delete('date_completed').to_date
 
       p = order_json.merge(created_at: created_at, updated_at: updated_at, paid_at: paid_at, completed_at: completed_at)
       p.reject { |k, v| !Order.column_names.include? k }
