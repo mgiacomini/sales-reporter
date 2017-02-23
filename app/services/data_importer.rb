01fa98576454f2b660111ee32e28ed4010b54e6d
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-23 10:59:06 -0300

    fix date on import

--- app/services/data_importer.rb
+++ app/services/data_importer.rb
@@ -4,10 +4,10 @@ module Services
   class DataImporter
 
     def create_order_params(order_json={})
-      created_at = order_json.delete('date_created').to_date
-      updated_at = order_json.delete('date_modified').to_date
-      paid_at = order_json.delete('date_paid').to_date
-      completed_at = order_json.delete('date_completed').to_date
+      created_at = Date.parse order_json.delete('date_created').to_date
+      updated_at = Date.parse order_json.delete('date_modified').to_date
+      paid_at = Date.parse order_json.delete('date_paid').to_date
+      completed_at = Date.parse order_json.delete('date_completed').to_date
 
       p = order_json.merge(created_at: created_at, updated_at: updated_at, paid_at: paid_at, completed_at: completed_at)
       p.reject { |k, v| !Order.column_names.include? k }
@@ -19,7 +19,7 @@ module Services
         params = create_order_params order_json
         orders_chunks << create_order_object(params.merge(wordpress: wordpress))
       end
-      Order.import orders_chunks, on_duplicate_key_update: [:status]
+      Order.import orders_chunks, on_duplicate_key_update: [:status, :created_at]
     end
 
     private
