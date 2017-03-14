b4052e01d7f6abe2fe83a49aa3117666b2c32cf0
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-03-14 13:08:18 -0300

    update gemfile

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,5 @@ class Order < ApplicationRecord
   belongs_to :wordpress
   scope :last_30_days, -> { where("wordpress_created_at >= ?", 30.days.ago) }
   scope :between_dates, -> (date_begin, date_end) { where(wordpress_created_at: date_begin.beginning_of_day..date_end.end_of_day) }
-
-  # Article.where(title: 'Rails 3').or(Article.where(title: 'Rails 4'))
   scope :converted, -> { where(status: 'processing').or(where(status: 'completed')) }
 end
